d9ae1ae243e95da8128454f656cd0a7621ee8a65
Daniel Young <daniel@deviateit.net>
2015-08-09 12:27:58 +1200

    Moved map zoom in to map func

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-(function() {
+(function(suburb_id) {
     var consumer_key = '1E1CEAEE21AC5CEF929105F5564520CA';
     var consumer_secret = '713EAA15AD958F0939E63BE7287C81D2%26';
 
-    var suburb_id = "99";
     var query_string = "suburb=" + suburb_id;
 
     var url = 'https://api.trademe.co.nz/v1/Search/Property/Residential.json?' +
