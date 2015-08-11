f9c222a803e8a90d7c657e113295991f8a407f60
Alan Pang <apan916@aucklanduni.ac.nz>
2015-08-11 13:35:57 +1200

    added infobox functionality

--- js/map_func.js
+++ js/map_func.js
@@ -5,35 +5,45 @@ function zoomMap(zoomLevel, lat, lng) {
 
 function find_properties_on_map(data) {
     var markers = [];
-        data.List.forEach(function (tm) {
-            var geo = tm.GeographicLocation;
-            var myLatlng2 = new google.maps.LatLng(geo.Latitude, geo.Longitude);
+    data.List.forEach(function (tm) {
+        var geo = tm.GeographicLocation;
+        var myLatlng2 = new google.maps.LatLng(geo.Latitude, geo.Longitude);
+        var address = tm.Address;
+        var price = tm.PriceDisplay;
+        var trademe_url = '<a href="https://trademe.co.nz/' + tm.ListingId + '">View on Trademe</a>';
 
-            var address = tm.Address;
-            var price = tm.PriceDisplay;
-            var trademe_url = '<a href="https://trademe.co.nz/' + tm.ListingId + '">View on Trademe</a>';
 
-
-            var marker = new google.maps.Marker({
-                position: myLatlng2,
-                map: map,
-                title: 'NewMarket House'
-
-            });
-
-            markers.push(marker);
-
-            var contentString = address + "<br>" + price + "<br>" + trademe_url;
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-
-            //zoomMap(14,-36.8683083, 174.7766063); // Centre on Newmarket
-
-            google.maps.event.addListener(marker, 'click', function () {
-                infowindow.open(map, marker);
-            });
+        var marker = new google.maps.Marker({
+            position: myLatlng2,
+            map: map,
+            title: 'NewMarket House'
         });
+
+        markers.push(marker);
+
+        var contentString = address + "<br>" + price + "<br>" + trademe_url;
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+
+        //zoomMap(14,-36.8683083, 174.7766063); // Centre on Newmarket
+
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.open(map, marker);
+            //infowindow.close();
+        });
+
+        google.maps.event.addListener(map, "click", function(event) {
+            infowindow.close();
+        });
+
+        function deleteMarkers() {
+            clearMarkers();
+            markers = [];
+        }
+    });
 
     var bounds = new google.maps.LatLngBounds();
     for(i=0;i<markers.length;i++) {
--- js/suburbPrices.js
+++ js/suburbPrices.js
@@ -1,23 +1,25 @@
 //Suburb data is calculated on residential property sales from the previous 3 months
-function suburb_price(num_sales, median_price, cv_difference, trademe_suburb_id) {
+function suburb_price(num_sales, median_price, cv_difference, trademe_suburb_id, regionName) {
     this.num_sales = num_sales;
     this.median_price = median_price;
     this.cv_difference = cv_difference;
     this.trademe_suburb_id = trademe_suburb_id;
+    this.regionName = regionName;
 }
 
-var newMarket = new suburb_price(10,581250,22,99);
+var newMarket = new suburb_price(10,581250,22,99,"Newmarket");
+var ponsonby = new suburb_price(35,1520000,13,83,"Ponsonby");
 var freemansBay = new suburb_price(25,1220000,16,88);
 var herneBay = new suburb_price(16,1430000,11,84);
 var kingsland = new suburb_price(16,724500,29,123);
 var parnell = new suburb_price(56,994000,10,41);
 var pointChevalier = new suburb_price(35,1052000,11,85);
-var ponsonby = new suburb_price(35,1520000,13,83);
 var sandringham = new suburb_price(63,736500,19,128);
 var westmere = new suburb_price(14,1155500,8,109);
 var edenTerrace = new suburb_price(30,378500,20,135);
 
-var suburb_data = [newMarket,freemansBay,herneBay,kingsland,parnell,pointChevalier,ponsonby,sandringham,westmere,edenTerrace];
+
+var suburb_data = [newMarket,ponsonby,freemansBay,herneBay,kingsland,parnell,pointChevalier,sandringham,westmere,edenTerrace];
 
 function updateSuburbInFor(suburb_id){
     $("#num_sales").empty();
@@ -27,4 +29,5 @@ function updateSuburbInFor(suburb_id){
     $("#num_sales").append("<li>" +result.num_sales + "</li>");
     $("#cv_price").append("<li>" +result.median_price + "</li>");
     $("#cv_difference").append("<li>" +result.cv_difference + "</li>");
-};
+    $("#regionName").append("<h2>" +result.regionName + "</h2>");
+}
